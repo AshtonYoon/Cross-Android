27719af70acf9adcdc3e2df3de588d3659bbd83f
Boxfox <boxfoxsg619@gmail.com>
2018-07-04 17:11:25 +0900

    Add API Service

---
+++ app/src/main/java/com/support/android/designlibdemo/Connector/WalletService.java
@@ -0,0 +1,50 @@
+package com.support.android.designlibdemo.Connector;
+
+import com.support.android.designlibdemo.Connector.model.Coin;
+import com.support.android.designlibdemo.Connector.model.TransactionStatus;
+import com.support.android.designlibdemo.Connector.model.Wallet;
+import com.support.android.designlibdemo.Connector.model.WalletFile;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface WalletService {
+
+    @GET("support/wallet/list")
+    Call<List<Coin>> getSupportedCoins();
+
+    @FormUrlEncoded
+    @POST("wallet/{symbol}/create")
+    Call<WalletFile> createWallet(@Path("symbol") String symbol, @Field("name") String name, @Field("description")  String description, @Field("password")  String password, @Field("major")boolean major);
+
+    @GET("wallet/list")
+    Call<List<Wallet>> getWalletList();
+
+    @GET("wallet/{symbol}/balance")
+    Call<String> getBalance(@Path("symbol") String symbol, @Query("address") String address);
+
+    @GET("wallet/{symbol}/price")
+    Call<String> getPrice(@Path("symbol") String symbol, @Query("address") String address);
+
+    @GET("wallet/{symbol}/price/all")
+    Call<String> getTotalPrice();
+
+    @GET("wallet/{symbol}/lookup")
+    Call<Wallet> getWalletInfo(@Path("symbol") String symbol, @Query("address") String address);
+
+    @GET("wallet/{symbol}/transaction")
+    Call<TransactionStatus> getTransactionStatus(@Path("symbol") String symbol, @Query("hash") String hash);
+
+    @GET("wallet/{symbol}/transaction/count")
+    Call<Integer> getTransactionCount(@Path("symbol") String symbol, @Query("address") String address);
+
+    @GET("wallet/{symbol}/transaction/list")
+    Call<List<TransactionStatus>> transactionList(@Path("symbol") String symbol, @Query("address") String address);
+}
---
+++ app/src/main/java/com/support/android/designlibdemo/Connector/model/WalletFile.java
@@ -0,0 +1,37 @@
+package com.support.android.designlibdemo.Connector.model;
+
+import io.realm.RealmObject;
+
+public class WalletFile extends RealmObject{
+    private String address;
+    private String name;
+    private String wallet;
+
+    public WalletFile(){
+
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(String wallet) {
+        this.wallet = wallet;
+    }
+}
