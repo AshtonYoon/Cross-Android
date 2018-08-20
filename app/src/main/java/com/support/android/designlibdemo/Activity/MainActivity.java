9ef64a161f96a11e2ffc54cd317a222a385be9c8
Ashton Yoon <y109384@naver.com>
2018-08-20 21:46:15 +0900

    clean resources, update ui

--- app/src/main/java/com/support/android/designlibdemo/Activity/MainActivity.java
+++ app/src/main/java/com/support/android/designlibdemo/Activity/MainActivity.java
@@ -26,83 +26,29 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import com.support.android.designlibdemo.Fragment.TransactionFragment;
-import com.support.android.designlibdemo.Fragment.WalletFragment;
+import com.support.android.designlibdemo.Adapters.MainPageAdapter;
+import com.support.android.designlibdemo.Fragments.ContactFragment;
+import com.support.android.designlibdemo.Fragments.TransactionFragment;
+import com.support.android.designlibdemo.Fragments.WalletFragment;
 import com.support.android.designlibdemo.R;
 
 /**
  * TODO
  */
 public class MainActivity extends AppCompatActivity {
-
-    private MainActivity.SectionsPagerAdapter mSectionsPagerAdapter;
-
-    private ViewPager mViewPager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
+        MainPageAdapter mainPageAdapter = new MainPageAdapter(getSupportFragmentManager());
+        mainPageAdapter.addFragment(new WalletFragment(), "지갑");
+        mainPageAdapter.addFragment(new TransactionFragment(), "트랜잭션");
+        mainPageAdapter.addFragment(new ContactFragment(), "주소록");
+        viewPager.setAdapter(mainPageAdapter);
 
-        mSectionsPagerAdapter = new MainActivity.SectionsPagerAdapter(getSupportFragmentManager());
-
-        mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-    }
-
-    public static class PlaceholderFragment extends Fragment {
-
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        public static MainActivity.PlaceholderFragment newInstance(int sectionNumber) {
-            MainActivity.PlaceholderFragment fragment = new MainActivity.PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-    }
-
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0: return WalletFragment.Companion.getInstance();
-                case 1: return TransactionFragment.Companion.getInstance();
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "지갑";
-                case 1:
-                    return "트랜젝션";
-                case 2:
-                    return "주소록";
-            }
-            return null;
-        }
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
